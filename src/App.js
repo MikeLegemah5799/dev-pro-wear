c9edefc89f4dd966383b3cae96296b755fd25686
Michael Legemah <michaellegemah@gmail.com>
2019-11-18 10:50:52 -0500

    created sass

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from "react";
 
 import "./App.css";
 
+import HomePage from "./homepage.component";
+
 function App() {
-  return <div>Hey hey</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
