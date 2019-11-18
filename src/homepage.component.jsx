a115622eecea5dacb39ac6a5f8d39a62db998e3e
Michael Legemah <michaellegemah@gmail.com>
2019-11-18 10:24:43 -0500

    Initial commit
    initial commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle"></span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
