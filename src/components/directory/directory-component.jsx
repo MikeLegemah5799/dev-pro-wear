50329f29fe496ea3cf886a35b62c323459ba6d13
Michael Legemah <michaellegemah@gmail.com>
2019-12-01 18:40:25 -0500

    added in menu styles and fonts

--- src/components/directory/directory-component.jsx
+++ src/components/directory/directory-component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import MenuItem from "../menu-item/menu-item";
+import MenuItem from "../menu-item/menu-item-component";
 
 import "./directory.styles.scss";
 
@@ -12,32 +12,37 @@ class Directory extends React.Component {
       sections: [
         {
           title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          imageUrl:
+            "https://images.pexels.com/photos/823723/pexels-photo-823723.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
           id: 1,
           linkUrl: "shop/hats"
         },
         {
           title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          imageUrl:
+            "https://images.pexels.com/photos/16170/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
           id: 2,
           linkUrl: "shop/jackets"
         },
         {
           title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          imageUrl:
+            "https://images.pexels.com/photos/2460438/pexels-photo-2460438.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
           id: 3,
           linkUrl: "shop/sneakers"
         },
         {
           title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          imageUrl:
+            "https://images.pexels.com/photos/1644888/pexels-photo-1644888.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
           size: "large",
           id: 4,
           linkUrl: "shop/womens"
         },
         {
           title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          imageUrl:
+            "https://images.pexels.com/photos/936043/pexels-photo-936043.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
           size: "large",
           id: 5,
           linkUrl: "shop/mens"
@@ -49,8 +54,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id }) => (
-          <MenuItem key={id} title={title} />
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
     );
